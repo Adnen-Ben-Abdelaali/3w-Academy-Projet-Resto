a00fd68a544b8f91e336e691f700a52e73f1f1cc
Adnen-Ben-Abdelaali <benabdelaaliadnen@gmail.com>
2020-03-12 17:01:43 +0100

    semi -- final two

---
+++ application/controllers/order/payment/PaymentController.class.php
@@ -0,0 +1,21 @@
+<?php
+
+  class PaymentCOntroler 
+  {
+    public function httpGetMethod(Http $http, array $queryFields)
+
+      // $queryFields : équivaleent au tableau $_GET
+
+    {   
+        return [];
+    }
+
+    public function httpPostMethod(Http $http, array $formFields)
+    {
+
+      // $formFields : équivalent au tableau $_POST
+
+        return [];
+    }
+  
+}
---
+++ application/controllers/order/payment/success/SuccessController.class.php
@@ -0,0 +1,21 @@
+<?php
+
+  class SuccessController
+  {
+    public function httpGetMethod(Http $http, array $queryFields)
+
+      // $queryFields : équivaleent au tableau $_GET
+
+    {   
+        return [];
+    }
+
+    public function httpPostMethod(Http $http, array $formFields)
+    {
+
+      // $formFields : équivalent au tableau $_POST
+
+        return [];
+    }
+  
+}
